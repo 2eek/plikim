91a1bee68e9a561ab5d8c96fd04105339991a7d5
2eek <khwan9404@yu.ac.kr>
2023-09-29 18:25:44 +0900

    init

---
+++ src/main/java/com/eek/kimpli/KimpliApplication.java
@@ -0,0 +1,13 @@
+package com.eek.kimpli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimpliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimpliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eek/kimpli/KimpliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eek.kimpli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimpliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

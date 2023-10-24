ac1875ad5c7e17fad430b22bdf2a8d86b74b240d
2eek <khwan9404@yu.ac.kr>
2023-10-24 18:45:22 +0900

    회원가입

--- src/main/java/com/eek/kimpli/controller/MemberController.java
+++ src/main/java/com/eek/kimpli/controller/MemberController.java
@@ -11,19 +11,27 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/board")
-public class BoardController {
+@RequestMapping("/member")//공통주소
+public class MemberController {
     private  final BoardService boardService;
     //생성자 주입방식으로 의존성주입받게됨
-    @GetMapping("/save")
-    public String saveForm(){return "board/save";}
+    //단순한 호출 GetMapping을 이용한다. URL에 매핑된 핸들러 필요하다.
+    @GetMapping("/memberlist")
+    public String saveForm(){
+        return "member/memberlist";}
 
-    @PostMapping("/save")
-    public String save(@ModelAttribute BoardDTO boardDTO){
-        System.out.println("boardDTO ="+ boardDTO);
-        boardService.save(boardDTO);
-        return "index";
+    @GetMapping("/memberjoin")
+    public String joinForm(){
+        return "member/memberjoinform";
     }
+
+
+//    @PostMapping("/save")
+//    public String save(@ModelAttribute BoardDTO boardDTO){
+//        System.out.println("boardDTO ="+ boardDTO);
+//        boardService.save(boardDTO);
+//        return "index";
+//    }
     @GetMapping("/list")
     public String findAll(Model model){
         // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다

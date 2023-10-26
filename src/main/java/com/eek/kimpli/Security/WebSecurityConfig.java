21615b7b0bfa6d2c3274cb86859c41ed2a45fa20
2eek <khwan9404@yu.ac.kr>
2023-10-26 15:03:35 +0900

    타이머 test

--- src/main/java/com/eek/kimpli/Security/WebSecurityConfig.java
+++ src/main/java/com/eek/kimpli/Security/WebSecurityConfig.java
@@ -7,12 +7,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.http.HttpSession;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+	 //비밀번호 암호화
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
    @Bean
    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,33 +31,48 @@ public class WebSecurityConfig {
         .authorizeRequests(authorizeRequests ->
             authorizeRequests
                 .antMatchers("/").permitAll()
+				//저장소에 Authentication없으면 로그인폼으로 돌림
                 .antMatchers("/board/**").authenticated()
                 .antMatchers("/static/**").permitAll()
                 .anyRequest().permitAll()
         )
         .formLogin(formLogin ->
             formLogin
-                .loginPage("/member/loginForm")
+				.loginPage("/member/loginForm")
                 .permitAll()
         )
         .logout(logout ->
-            logout
-                .permitAll()
-        );
+			logout
+			// 로그아웃 요청을 처리할 URL 설정
+			.logoutUrl("/logout")
+			// 로그아웃 성공 시 리다이렉트할 URL 설정
+			.logoutSuccessUrl("/")
+//			// 로그아웃 핸들러 추가 (세션 무효화 처리)
+			.addLogoutHandler((request, response, authentication) -> {
+				HttpSession session = request.getSession();
+				session.invalidate();
+			})
+//			// 로그아웃 성공 핸들러 추가 (리다이렉션 처리)
+//			.logoutSuccessHandler((request, response, authentication) ->
+//					response.sendRedirect("/"))
+//			// 로그아웃 시 쿠키 삭제 설정 (예: "remember-me" 쿠키 삭제)
+//			.deleteCookies("remember-me")
+                );
+
     return http.build();
 }
 
 
 
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user);
+//	}
 }

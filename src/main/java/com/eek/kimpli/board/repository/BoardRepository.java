d0157a7a2357164bc12d088975248c5971dc12ae
2eek <khwan9404@yu.ac.kr>
2023-10-28 23:05:21 +0900

    기본 게시판 작성 완2 글 상세조회 가능

--- src/main/java/com/eek/kimpli/board/repository/BoardRepository.java
+++ src/main/java/com/eek/kimpli/board/repository/BoardRepository.java
@@ -1,7 +1,7 @@
 package com.eek.kimpli.board.repository;
 
-import com.eek.kimpli.entity.BoardEntity;
+import com.eek.kimpli.board.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoardRepository extends JpaRepository<BoardEntity, Long >{
+public interface BoardRepository extends JpaRepository<Board, Long > {
 }
--- src/main/java/com/eek/kimpli/repository/Board1Repository.java
+++
@@ -1,7 +0,0 @@
-package com.eek.kimpli.repository;
-
-import com.eek.kimpli.model.Board1;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Board1Repository extends JpaRepository<Board1, Long > {
-}

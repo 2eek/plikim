ccdbda1858441096eda420b7f9a029342de7f9f6
2eek <khwan9404@yu.ac.kr>
2023-11-02 20:34:54 +0900

    스프링시큐리티 로그인 회원가입 비밀번호 암호화 jpa 자동 테이블 생성 테스트 완

---
+++ src/main/java/com/eek/kimpli/tabletest/Member1.java
@@ -0,0 +1,21 @@
+package com.eek.kimpli.tabletest;
+
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity(name = "Members")
+public class Member1 {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+}

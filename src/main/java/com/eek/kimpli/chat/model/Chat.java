d11e70406688d969c5a529ec34d366c92aa86aa9
2eek <khwan9404@yu.ac.kr>
2023-11-03 19:24:21 +0900

    채팅 적용 완. -> 회원 별 채팅 가능하게 구현하기

--- src/main/java/com/eek/kimpli/chat/model/Chat.java
+++ src/main/java/com/eek/kimpli/chat/model/Chat.java
@@ -15,5 +15,6 @@ public class Chat {
     private String msg;
     private String sender;//보내는 사람
     private String receiver;//받는 사람
+    private Integer roomNum; //방 번호
     private LocalDateTime createdAt;
 }

3def06937405b530341126c0f88a8392781e5da1
2eek <khwan9404@yu.ac.kr>
2023-11-09 20:08:11 +0900

    채팅창에 nav 적용완 배포용

--- src/main/resources/static/js/chatJs.js
+++ src/main/resources/static/js/chatJs.js
@@ -9,9 +9,9 @@ document.querySelector("#username").innerHTML = username;
 
 // SSE 연결하기. 객체 생성. 크로스 오리진 자바스크립트 요청은 서버쪽에서 봉쇄하고 있다. -> 서버에서 처리함
 //const eventSource = new EventSource(`http://localhost:9090/chat/roomNum/${roomNum}`);
-//const eventSource = new EventSource(`https://plikim.com/chat/roomNum/${roomNum}`);
+const eventSource = new EventSource(`https://plikim.com/chat/roomNum/${roomNum}`);
 //const eventSource = new EventSource(`http://localhost:9090/sender/ssar/receiver/cos`);
-const eventSource = new EventSource(`http://localhost:9090/chat/roomNum/${roomNum}`);
+//const eventSource = new EventSource(`http://localhost:9090/chat/roomNum/${roomNum}`);
 
 
 eventSource.onmessage = (event) => {
@@ -91,7 +91,7 @@ async function addMessage() {
     };
 
     try {//"http://localhost:9090/chat" "https://plikim.com/chat"
-        const response = await fetch( "http://localhost:9090/chat", {
+        const response = await fetch( "https://plikim.com/chat", {
             method: "post",
             body: JSON.stringify(chat),
             headers: {

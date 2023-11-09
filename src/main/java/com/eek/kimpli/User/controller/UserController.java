33e5f8f98c544618f97c22c21f4d823ad2a56670
2eek <khwan9404@yu.ac.kr>
2023-11-09 19:56:14 +0900

    채팅창에 nav 적용완

--- src/main/java/com/eek/kimpli/User/controller/UserController.java
+++ src/main/java/com/eek/kimpli/User/controller/UserController.java
@@ -5,7 +5,6 @@ import com.eek.kimpli.User.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
-public class AccountController {
+public class UserController {
 
     @Autowired
     private UserService userService;
--- src/main/java/com/eek/kimpli/controller/HomeController.java
+++ src/main/java/com/eek/kimpli/controller/HomeController.java
@@ -1,12 +1,39 @@
 package com.eek.kimpli.controller;
 
+import com.eek.kimpli.board.model.Board;
+import com.eek.kimpli.board.repository.BoardRepository;
+import com.eek.kimpli.board.validator.BoardValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String index(){
+//    @GetMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
+        @Autowired //DI일어남. 여기에 인스턴스가 들어옴
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
+
+        @GetMapping("/")
+    public String list(Model model, @PageableDefault(size = 3) Pageable pageable,
+                       @RequestParam(required = false,defaultValue = "") String searchText) {
+        Page<Board> boards = boardRepository.findByTitleContainingOrContentContaining(searchText,searchText,pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endpage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endpage);
+        model.addAttribute("boards", boards);
         return "index";
     }
 }

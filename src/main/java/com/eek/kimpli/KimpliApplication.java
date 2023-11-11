9259574c2c25ae0874dd097d4d87e783d04c5eaa
2eek <khwan9404@yu.ac.kr>
2023-11-11 20:44:47 +0900

    index에 회원 화면 출력

--- src/main/java/com/eek/kimpli/KimpliApplication.java
+++ src/main/java/com/eek/kimpli/KimpliApplication.java
@@ -1,17 +1,10 @@
 package com.eek.kimpli;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Controller;
+
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "com.eek.kimpli.**")
-//@EntityScan(basePackages = "com.eek.kimpli.**")
-//@EnableMongoRepositories
 public class KimpliApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/eek/kimpli/chat/repository/ChatRepository.java
+++ src/main/java/com/eek/kimpli/chat/repository/ChatRepository.java
@@ -4,7 +4,6 @@ import com.eek.kimpli.chat.model.Chat;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.mongodb.repository.Tailable;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 

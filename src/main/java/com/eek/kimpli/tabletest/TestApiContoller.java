a458280ff21d43f22b77c7d849f7787e9d424a21
2eek <khwan9404@yu.ac.kr>
2023-11-20 12:29:06 +0900

    회원리스트 랜덤 출력 완료

--- src/main/java/com/eek/kimpli/tabletest/TestApiContoller.java
+++ src/main/java/com/eek/kimpli/tabletest/TestApiContoller.java
@@ -1,26 +1,26 @@
-package com.eek.kimpli.tabletest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-@RequestMapping("/test")
-public class TestApiContoller {
-
-    @Autowired
-    Memberrepository1 memberRepository1;
-
-    @GetMapping
-    String test(){
-        Member1 member1 = Member1.builder()
-                .id(1L)
-                .name("스프링")
-                .build();
-        System.out.println(member1.toString());
-        memberRepository1.save(member1);
-
-        return "test";
-    }
-}
+//package com.eek.kimpli.tabletest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@RestController
+//@RequestMapping("/test")
+//public class TestApiContoller {
+//
+//    @Autowired
+//    Memberrepository1 memberRepository1;
+//
+//    @GetMapping
+//    String test(){
+//        Member1 member1 = Member1.builder()
+//                .id(1L)
+//                .name("스프링")
+//                .build();
+//        System.out.println(member1.toString());
+//        memberRepository1.save(member1);
+//
+//        return "test";
+//    }
+//}

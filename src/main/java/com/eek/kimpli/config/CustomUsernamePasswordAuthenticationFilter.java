a8ce8f0dfd6cc3a0d673179a1192564809b937d8
2eek <khwan9404@yu.ac.kr>
2023-11-24 21:18:58 +0900

    세션 처리중

--- src/main/java/com/eek/kimpli/config/CustomUsernamePasswordAuthenticationFilter.java
+++ src/main/java/com/eek/kimpli/config/CustomUsernamePasswordAuthenticationFilter.java
@@ -1,13 +1,13 @@
-package com.eek.kimpli.config;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    public CustomUsernamePasswordAuthenticationFilter() {
-        super();
-        setUsernameParameter("userId"); // 사용자명 파라미터를 userId로 설정
-    }
-
-    // 다른 설정이나 커스텀 로직이 필요한 경우 여기에 추가할 수 있음
-}
+//package com.eek.kimpli.config;
+//
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//public class CustomUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+//
+//    public CustomUsernamePasswordAuthenticationFilter() {
+//        super();
+//        setUsernameParameter("userId"); // 사용자명 파라미터를 userId로 설정
+//    }
+//
+//    // 다른 설정이나 커스텀 로직이 필요한 경우 여기에 추가할 수 있음
+////}

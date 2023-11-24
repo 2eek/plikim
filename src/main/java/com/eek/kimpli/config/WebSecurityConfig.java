9bf259f74dfa8e4369aa15a511afb632d425c461
2eek <khwan9404@yu.ac.kr>
2023-11-24 16:15:08 +0900

    user table 수정

--- src/main/java/com/eek/kimpli/config/WebSecurityConfig.java
+++ src/main/java/com/eek/kimpli/config/WebSecurityConfig.java
@@ -10,7 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -21,6 +23,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     final DataSource dataSource;
     final PasswordEncoder passwordEncoder;
+   //로그인 시 userId 칼럼명 사용
+      // 주입 받은 필터를 빈으로 등록
+    @Bean
+    public CustomUsernamePasswordAuthenticationFilter customUsernamePasswordAuthenticationFilter() throws Exception {
+        CustomUsernamePasswordAuthenticationFilter filter = new CustomUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(authenticationManagerBean());
+        return filter;
+    }
 
   @Bean
 public SessionRegistry sessionRegistry() {
@@ -30,7 +40,7 @@ public SessionRegistry sessionRegistry() {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+      .addFilterAt(customUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class) // 필터 빈을 호출하여 등록                .csrf().disable()
                 .authorizeRequests()
                         //누구나 접근 가능
                         .antMatchers("/", "/account/register", "/css/**", "/js/**","/api/**" ,"/img/**","/static/**","/member/memberjoin").permitAll()
@@ -39,7 +49,7 @@ public SessionRegistry sessionRegistry() {
                 .anyRequest().permitAll()
                         .and()
                 .formLogin()
-                        .loginPage("/member/loginForm")
+                        .loginPage("/member/loginForm") //매핑uri
                         .permitAll()
                        .loginProcessingUrl("/login") // 스프링 시큐리티에서 처리하기위한 주소. /login post방식.
                      .defaultSuccessUrl("/")
@@ -58,20 +68,25 @@ public SessionRegistry sessionRegistry() {
 
 
     }
+
+    //로그인 과정에서의 스프링 내부에서 인증수행한다.
 @Autowired
 public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      //jdbc이용
     auth.jdbcAuthentication()
         .dataSource(dataSource)
         .passwordEncoder(passwordEncoder)
-        .usersByUsernameQuery("select username, password, enabled "
+            //인증처리 (로그인처리)Authentication
+        .usersByUsernameQuery("select user_id, password, enabled "
                 + "from user "
-                + "where username = ?")
-        .authoritiesByUsernameQuery("select u.username, r.name "
-                + "from user_role ur inner join user u on ur.user_id = u.id "
-                + "inner join role r on ur.role_id = r.id "
-                + "where u.username = ?");
-}
+                + "where user_id = ?")
+            //인가처리 (사용자 권한) Authorization
+        .authoritiesByUsernameQuery("select u.user_id, r.name "
+                + "from user_role ur inner join user u on ur.user_index = u.index "
+                + "inner join role r on ur.role_index = r.index "
+                + "where u.user_id = ?");
 
+}
 
 
 

a3fc391396859c07bcacf371311949cb6f1d5b5b
2eek <khwan9404@yu.ac.kr>
2023-12-04 18:30:26 +0900

    ㄹ그인화면 preloading

--- src/main/java/com/eek/kimpli/member/service/KakaoLoginServiceImpl.java
+++ src/main/java/com/eek/kimpli/member/service/KakaoLoginServiceImpl.java
@@ -35,7 +35,9 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          URL url = new URL(reqURL);
          //주어진 URL에 대한 HttpURLConnection 객체를 생성하고 연결
          HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+         //데이터를 요청 본문에 실어서 서버로 전송하는데(POST요청), 이때 출력이 필요
          conn.setRequestMethod("POST");
+         //출력을 허용
          conn.setDoOutput(true);
 
          //POST 요청. 서버로 데이터를 전송 -> HttpURLConnection의 출력 스트림을 얻어와서-> 문자열로 조작
@@ -44,8 +46,8 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          StringBuilder sb = new StringBuilder();
          sb.append("grant_type=authorization_code");
          sb.append("&client_id=1e411be4c9538cd8fc4f1b4c817968b4"); //본인이 발급받은 key
-         //sb.append("&redirect_uri=https://plikim.com/kakaologin"); // 본인이 설정한 주소
-         sb.append("&redirect_uri=http://localhost:9090/kakaologin"); // 본인이 설정한 주소
+         sb.append("&redirect_uri=https://plikim.com/kakaologin"); // 본인이 설정한 주소
+         //sb.append("&redirect_uri=http://localhost:9090/kakaologin"); // 본인이 설정한 주소
          sb.append("&code=" + authorize_code);
          bw.write(sb.toString());
 
@@ -54,7 +56,6 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
 
          // 요청이 정상 도달-> 200
          int responseCode = conn.getResponseCode();
-         System.out.println("responseCode : " + responseCode);
          // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
          BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
          String line = "";
@@ -62,7 +63,6 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          while ((line = br.readLine()) != null) {
             result += line;
          }
-         System.out.println("response body'토큰.' : " + result);
 
        //JSON파싱 객체 생성
          // Gson 라이브러리의 JsonParser와 JsonElement 대신 Jackson 라이브러리의 ObjectMapper와 JsonNode를 사용
@@ -72,8 +72,7 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          access_Token = jsonNode.get("access_token").asText();
          //refresh 토큰은 access 토큰 만료후 자동으로 접근 토큰을 갱신
          refresh_Token = jsonNode.get("refresh_token").asText();
-         System.out.println("access_token : " + access_Token);
-         System.out.println("refresh_token : " + refresh_Token);
+
 
          //메모리 누수를 방지
          br.close();
@@ -86,9 +85,6 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
       token.put("access_token", access_Token);
       token.put("refresh_token", refresh_Token);
 
-      System.out.println("카카오서비스 맨 마지막 ");
-      System.out.println("access_token : " + access_Token);
-      System.out.println("refresh_token : " + refresh_Token);
       return token;
    }
 
@@ -104,7 +100,6 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          conn.setRequestMethod("GET");
          conn.setRequestProperty("Authorization", "Bearer " + access_Token);
          int responseCode = conn.getResponseCode();
-         System.out.println("responseCode : " + responseCode);
          BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
          String line = "";
          String result = "";
@@ -127,7 +122,6 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          Long id = jsonNode.get("id").asLong();
          userInfo.put("id", id);
          userInfo.put("nickname", nickname);
-         System.out.println("@@@@@@닉네임@@@@@"+nickname);
          userInfo.put("email", email);
          userInfo.put("thumbnailImage", thumbnailImage);
          userInfo.put("profileImage", profileImage);
@@ -135,14 +129,7 @@ public class KakaoLoginServiceImpl implements KakaoLoginService {
          userInfo.put("access_Token", access_Token);
          userInfo.put("refresh_Token", refresh_Token);
          userInfo.put("refresh_Token", profileImage);
-         //userInfo.put("gender", gender);
-         //userInfo.put("birthday", birthday);
-		/* userInfo.put("birthday", birthday); */
-         System.out.println("********");
-         System.out.println("###id#### : " + id);
-         System.out.println("###프로필### : " + profileImage);
-         System.out.println("###엑세스토큰### : " + access_Token);
-         System.out.println("###리프레쉬토큰### : " + refresh_Token);
+
          userInfo.put("id", id);
 
       } catch (IOException e) {

a971eb29e1b0b0f160d0915e6b137b6fa826020d
2eek <khwan9404@yu.ac.kr>
2023-12-04 18:30:26 +0900

    ㄹ그인화면 preloading

--- src/main/java/com/eek/kimpli/member/service/KakaoLoginService.java
+++ src/main/java/com/eek/kimpli/member/service/KakaoLoginService.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 public interface KakaoLoginService {
 
-public	Map<String, String> getAccessToken (String authorize_code);
-public MemberDTO getUserInfo(String access_Token, String refresh_Token) ;
-
+public	Map<String, String> getAccessToken (String authorize_code); // 매개변수로 인가 코드를 받아오고, Map<String, String> 형태로 액세스 토큰과 리프레시 토큰을 반환
+public MemberDTO getUserInfo(String access_Token, String refresh_Token) ;//   액세스 토큰과 리프레시 토큰을 받아오고, MemberDTO라는 데이터 전송 객체를 반환
 }

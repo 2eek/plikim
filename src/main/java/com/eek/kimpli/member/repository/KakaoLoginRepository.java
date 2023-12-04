f3ad03c023d8c17590a0dcb4952e1df2ea7f8d52
2eek <khwan9404@yu.ac.kr>
2023-12-04 20:42:46 +0900

    카카오 로그인

--- src/main/java/com/eek/kimpli/member/repository/KakaoLoginRepository.java
+++ src/main/java/com/eek/kimpli/member/repository/KakaoLoginRepository.java
@@ -2,6 +2,7 @@ package com.eek.kimpli.member.repository;
 
 
 import com.eek.kimpli.member.dto.MemberDTO;
+import com.eek.kimpli.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,28 +14,15 @@ import java.util.HashMap;
 public class KakaoLoginRepository {
     private final SqlSessionTemplate sql;
 
-
-//    public int save(MemberDTO memberDTO){
-//        System.out.println("memberDTO = " + memberDTO);
-//        return sql.insert("Member.save",memberDTO);
-//    }
-      //카카오 로그인 시 정보 저장
-    public int insertKakaoLogin(HashMap<String, Object> userInfo){
-        //파인드 하고 인서트 한다.
-        return sql.insert("Member.insertKakaoLogin",userInfo);
+    public int insertKakaoLogin(HashMap<String, Object> userInfo) {
+        return sql.insert("User.insertKakaoLogin", userInfo);
     }
 
-	 public int updateKakaoLogin(HashMap<String, Object> userInfo){
-        return sql.insert("Member.updateKakoLogin",userInfo);
-     }
+    public int updateKakaoLogin(HashMap<String, Object> userInfo) {
+        return sql.update("User.updateKakoLogin", userInfo);
+    }
 
-	 public MemberDTO findKakao(HashMap<String, Object> userInfo){
-
-        return sql.selectOne("Member.findKakao",userInfo);
-     }
-
-//    public MemberDTO  find(HashMap<String, Object> userInfo) {
-//        System.out.println("memberDTO = "+ userInfo);
-//        return sql.selectOne("Member.login",userInfo);  //MemberMapper.xml과 연결됨
-//    }
+    public User findKakao(HashMap<String, Object> userInfo) {
+        return sql.selectOne("User.findKakao", userInfo);
+    }
 }

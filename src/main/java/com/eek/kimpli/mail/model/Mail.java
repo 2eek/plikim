7d83c13c459647103ccca67c9351be8e71481805
2eek <khwan9404@yu.ac.kr>
2023-12-06 11:39:31 +0900

    비밀번호 찾기 완료

---
+++ src/main/java/com/eek/kimpli/mail/model/Mail.java
@@ -0,0 +1,14 @@
+package com.eek.kimpli.mail.model;
+
+import lombok.Data;
+
+
+@Data
+public class Mail {
+
+    private String receiver;
+    private String title;
+    private String content;
+}
+
+

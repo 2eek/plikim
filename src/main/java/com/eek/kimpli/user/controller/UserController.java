ea7d8b2ea317bdacd3d7ddabf0e3201a547fe3f3
2eek <khwan9404@yu.ac.kr>
2023-12-18 20:20:11 +0900

    카톡 이미지 사진 적용

--- src/main/java/com/eek/kimpli/user/controller/UserController.java
+++ src/main/java/com/eek/kimpli/user/controller/UserController.java
@@ -182,17 +182,17 @@ public class UserController {
                 String savePath = "/home/serve/plikim_img/user_profileImg/" + loggedInUser.getStoredFileName();
 
                 System.out.println("저장경로" + savePath);
-System.out.println("업데잍트 하나?요가눈> "+loggedInUser.getStoredFileName());
+
 // 파일 저장 (한 번만 저장하도록 수정)
                 FileService.saveFile(profileFile.getBytes(), savePath);
 
 // 업로드된 파일을 서버에 저장
 // 아래 라인에서 'file' 대신 'profileFile'을 사용해야 합니다.
 //                FileService.saveProfileImage(profileFile, "src/main/resources/static/profileImg", loggedInUser.getStoredFileName());
-   System.out.println("업데잍트 하나?그러면 이름은?1 "+loggedInUser.getStoredFileName());
+
 // 업데이트 로직 호출
                 userService.updateUserInfo(loggedInUser);
-                System.out.println("업데잍트 하나?그러면 이름은?2 "+loggedInUser.getStoredFileName());
+
             }
 
         } catch (IOException e) {

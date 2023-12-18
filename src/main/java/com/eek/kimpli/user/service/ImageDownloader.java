6cf4de8e5e849282f461d04797064f26d22654ff
2eek <khwan9404@yu.ac.kr>
2023-12-18 20:51:07 +0900

    카톡 이미지 사진 적용

---
+++ src/main/java/com/eek/kimpli/user/service/ImageDownloader.java
@@ -0,0 +1,43 @@
+package com.eek.kimpli.user.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
+public class ImageDownloader {
+
+    public static void imgServerSave(String imageUrl) {
+
+        String savePath = "/home/serve/plikim_img/user_profileImg/";
+
+        try {
+            // 이미지 다운로드
+            downloadImage(imageUrl, savePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void downloadImage(String imageUrl, String savePath) throws IOException {
+        URL url = new URL(imageUrl);
+
+        // 이미지 다운로드를 위한 InputStream
+        try (InputStream in = url.openStream()) {
+            // 저장할 경로와 파일명 지정
+
+             // 파일명을 UUID를 이용하여 랜덤하게 생성
+        String fileName = UUID.randomUUID().toString() + ".jpg";
+
+        // 저장할 경로와 파일명 지정
+        Path filePath = Path.of(savePath + fileName);
+
+            // 이미지를 파일로 저장
+            Files.copy(in, filePath, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
+

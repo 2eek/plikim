e2810e334435441dd2741a4931025f1df57e224b
2eek <khwan9404@yu.ac.kr>
2023-12-19 18:39:34 +0900

    채팅시 이미지

--- src/main/resources/static/js/chatJs.js
+++ src/main/resources/static/js/chatJs.js
@@ -96,10 +96,10 @@ let profileImageElement = document.querySelector(".profile_name img");
 
 // 대화 상자 엘리먼트 생성
 let messageBox = document.createElement("div");
+messageBox.appendChild(profileImageElement.cloneNode(true));  // 이미지 복제해서 추가
 messageBox.innerHTML = getReceiveMsgBox(data);
 
 // 이미지와 대화 상자를 받은 상자에 추가
-messageBox.appendChild(profileImageElement.cloneNode(true));  // 이미지 복제해서 추가
 receivedBox.appendChild(messageBox);
 
 // 채팅 박스에 추가

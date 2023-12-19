8334ad053c646f65d5ca11e75e944402b63a04e9
2eek <khwan9404@yu.ac.kr>
2023-12-19 17:05:27 +0900

    채팅시 이미지

--- src/main/resources/static/js/chatJs.js
+++ src/main/resources/static/js/chatJs.js
@@ -11,8 +11,8 @@ var roomNum = [username, receiver].sort().join('');
 document.querySelector("#username").innerHTML = receiver;
 
 // SSE 연결하기. 객체 생성. 크로스 오리진 자바스크립트 요청은 서버쪽에서 봉쇄하고 있다. -> 서버에서 처리함
-   //const eventSource = new EventSource(`https://plikim.com/chat/roomNum/${roomNum}`);
- const eventSource = new EventSource(`http://localhost:9090/chat/roomNum/${roomNum}`);
+   const eventSource = new EventSource(`https://plikim.com/chat/roomNum/${roomNum}`);
+ //const eventSource = new EventSource(`http://localhost:9090/chat/roomNum/${roomNum}`);
 
 eventSource.onmessage = (event) => {
 	//console.log(1,event);
@@ -112,6 +112,11 @@ function initYourMessage(data) {
     receivedBox.appendChild(profileImageElement);
     receivedBox.appendChild(messageBox);
 
+    // 이미지를 왼쪽에 위치시키기
+    profileImageElement.style.position = "absolute";
+    profileImageElement.style.left = "0";
+	profileImageElement.style.display = "block";
+
     // 채팅 박스에 추가
     chatBox.appendChild(receivedBox);
 
@@ -132,8 +137,8 @@ async function addMessage() {
 	};
 
 	try {
-		//const response = await fetch("https://plikim.com/chat", {
-			 const response = await fetch( "http://localhost:9090/chat", {
+		const response = await fetch("https://plikim.com/chat", {
+			// const response = await fetch( "http://localhost:9090/chat", {
 			method: "post",
 			body: JSON.stringify(chat),
 			headers: {

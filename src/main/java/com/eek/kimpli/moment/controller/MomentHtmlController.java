91c14e3f14a5d4e70ea72de86dc4d854ce496e4c
2eek <khwan9404@yu.ac.kr>
2023-12-20 21:27:16 +0900

    모멘트 개발 다중 이미지 첨부

--- src/main/java/com/eek/kimpli/moment/controller/MomentHtmlController.java
+++ src/main/java/com/eek/kimpli/moment/controller/MomentHtmlController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MomentHtmlController {
-     @GetMapping("moment/list")
+     @GetMapping("/moment/list")
     public String list() {
         return "/moment/list"; // HTML View를 반환하는 메소드
     }

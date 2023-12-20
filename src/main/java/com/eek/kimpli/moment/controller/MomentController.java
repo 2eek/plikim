e222d87d89d73284fddc85d4e728944e4b0fcfe1
2eek <khwan9404@yu.ac.kr>
2023-12-20 15:44:02 +0900

    모멘트 기능개발

--- src/main/java/com/eek/kimpli/moment/controller/MomentController.java
+++ src/main/java/com/eek/kimpli/moment/controller/MomentController.java
@@ -1,13 +1,40 @@
 package com.eek.kimpli.moment.controller;
 
+
+import com.eek.kimpli.moment.dto.Moment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequestMapping("/moment")
 public class MomentController {
 
-    @GetMapping("/moment/list")
-    public String list (){
-        return "/moment/list";
+    private final List<Moment> data = generateSampleData();
+
+    @GetMapping("/list")
+    public String list() {
+        return "/moment/list"; // HTML View를 반환하는 메소드
     }
+
+    @ResponseBody
+    @GetMapping("/data")
+    public List<Moment> getData() {
+        return data; // JSON 데이터를 반환하는 메소드
+    }
+  @ResponseBody
+private List<Moment> generateSampleData() {
+    List<Moment> sampleData = new ArrayList<>();
+
+    for (int i = 1; i <= 10; i++) {
+        sampleData.add(new Moment(i, "Moment " + i));
+    }
+
+    return sampleData;
+}
 }
--- src/main/java/com/eek/kimpli/moment/dto/Moment.java
+++ src/main/java/com/eek/kimpli/moment/dto/Moment.java
@@ -1,4 +1,24 @@
 package com.eek.kimpli.moment.dto;
 
 public class Moment {
+    private int id;
+    private String text;
+
+    // 생성자, 게터, 세터 등...
+
+    // 예시로 간단하게 생성자와 게터, 세터 추가
+    public Moment(int id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    // 세터 등도 필요에 따라 추가할 수 있음
 }

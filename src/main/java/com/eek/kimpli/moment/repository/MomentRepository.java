b656e0b71fe51a795726e42d9a73d00bb3190eac
2eek <khwan9404@yu.ac.kr>
2023-12-22 22:52:25 +0900

    모멘트 save기능개발 db저장완

--- src/main/java/com/eek/kimpli/moment/repository/MomentRepository.java
+++ src/main/java/com/eek/kimpli/moment/repository/MomentRepository.java
@@ -3,15 +3,16 @@ package com.eek.kimpli.moment.repository;
 import com.eek.kimpli.moment.model.Moment;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 
 public interface MomentRepository extends JpaRepository<Moment, Long > {
 //Moment의 momentImg 필드를 이용
-    @EntityGraph(attributePaths = "momentImg")
+   @EntityGraph(attributePaths = "momentImg")
+    @Query("SELECT m FROM Moment m ORDER BY m.createdDate DESC")  // 여기에 정렬을 추가
     List<Moment> findAll();
-
 
 
 

dfb0ae2fa279be714d14f2257175b188938279fe
2eek <khwan9404@yu.ac.kr>
2023-12-28 16:57:32 +0900

    회원 마이페이지

--- src/main/java/com/eek/kimpli/chat/contorller/ChatViewController.java
+++ src/main/java/com/eek/kimpli/chat/contorller/ChatViewController.java
@@ -21,65 +21,65 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ChatViewController {
 
-     private final ChatRepository chatRepository;
-     private final UserService userService;
+    private final ChatRepository chatRepository;
+    private final UserService userService;
 
- @GetMapping("/mychatrooms")
- public String chatRoomList(Model model) {
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    String userName = authentication.getName(); // 기본적으로는 authentication에서 가져오기
+    @GetMapping("/mychatrooms")
+    public String chatRoomList(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName(); // 기본적으로는 authentication에서 가져오기
 
-    if (userName == null || userName.isEmpty()) {
-        Object principal = authentication.getPrincipal();
-        if (principal instanceof Map) {
-            // principal이 Map인 경우, Map에서 username을 추출하여 userName으로 사용
-            userName = ((Map<String, String>) principal).get("username");
-            model.addAttribute("userSession", userName);
-            System.out.println("사용자 아이디: " + userName);
-        } else {
-            System.out.println("UserDetails가 아닌 Principal입니다: " + principal);
+        if (userName == null || userName.isEmpty()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof Map) {
+                // principal이 Map인 경우, Map에서 username을 추출하여 userName으로 사용
+                userName = ((Map<String, String>) principal).get("username");
+                model.addAttribute("userSession", userName);
+                System.out.println("사용자 아이디: " + userName);
+            } else {
+                System.out.println("UserDetails가 아닌 Principal입니다: " + principal);
+            }
         }
+
+        final String finalUserName = userName; // final 또는 effectively final로 선언
+
+        Flux<Chat> chatRoomFlux = chatRepository.findAll();
+        List<Chat> chatRoomList = chatRoomFlux.collectList().block();
+
+        if (chatRoomList != null) {
+            Set<String> uniqueRoomNums = new HashSet<>();
+
+            chatRoomList = chatRoomList.stream()
+                    .filter(chat -> chat.getRoomNum() != null && chat.getRoomNum().contains(finalUserName))
+                    .sorted(Comparator.comparing(Chat::getCreatedAt).reversed())
+                    .filter(chat -> uniqueRoomNums.add(chat.getRoomNum()))
+                    .map(chat -> {
+                        try {
+                            // 메시지 복호화
+                            String decryptedMessage = AesUtil.aesCBCDecode(chat.getMsg());
+                            chat.setMsg(decryptedMessage);
+                        } catch (Exception e) {
+                            e.printStackTrace(); // 복호화 실패 시 예외 처리
+                        }
+                        return chat;
+                    })
+                    .collect(Collectors.toList());
+        }
+
+        model.addAttribute("userSession", userName);
+        model.addAttribute("chatList", chatRoomList);
+        System.out.println("이름: " + userName);
+
+        List<String> roomNumList = new ArrayList<>();
+        for (Chat chat : chatRoomList) {
+            String roomNum = chat.getRoomNum();
+            System.out.println("roomNum: " + roomNum);
+            roomNumList.add(roomNum);
+        }
+        model.addAttribute("roomNumList", roomNumList);
+
+        return "chat/mychatrooms";
     }
-
-    final String finalUserName = userName; // final 또는 effectively final로 선언
-
-    Flux<Chat> chatRoomFlux = chatRepository.findAll();
-    List<Chat> chatRoomList = chatRoomFlux.collectList().block();
-
-    if (chatRoomList != null) {
-        Set<String> uniqueRoomNums = new HashSet<>();
-
-        chatRoomList = chatRoomList.stream()
-                .filter(chat -> chat.getRoomNum() != null && chat.getRoomNum().contains(finalUserName))
-                .sorted(Comparator.comparing(Chat::getCreatedAt).reversed())
-                .filter(chat -> uniqueRoomNums.add(chat.getRoomNum()))
-                .map(chat -> {
-                    try {
-                        // 메시지 복호화
-                        String decryptedMessage = AesUtil.aesCBCDecode(chat.getMsg());
-                        chat.setMsg(decryptedMessage);
-                    } catch (Exception e) {
-                        e.printStackTrace(); // 복호화 실패 시 예외 처리
-                    }
-                    return chat;
-                })
-                .collect(Collectors.toList());
-    }
-
-    model.addAttribute("userSession", userName);
-    model.addAttribute("chatList", chatRoomList);
-    System.out.println("이름: " + userName);
-
-    List<String> roomNumList = new ArrayList<>();
-    for (Chat chat : chatRoomList) {
-        String roomNum = chat.getRoomNum();
-        System.out.println("roomNum: " + roomNum);
-        roomNumList.add(roomNum);
-    }
-    model.addAttribute("roomNumList", roomNumList);
-
-    return "chat/mychatrooms";
-}
 
     // 중복된 userName을 제거한 roomNum을 반환하는 메서드
     private String removeUserName(String roomNum, String userName) {
@@ -90,11 +90,11 @@ public class ChatViewController {
     public String chat(Model model, @RequestParam(name = "userId") String userId) {
         model.addAttribute("userId", userId); // userId를 모델에 추가' 대화 상대방.
         User userById = userService.getUserById(userId);
-model.addAttribute("userinfo", userById); // userId를 모델에 추가' 대화 상대방.
+        model.addAttribute("userinfo", userById); // userId를 모델에 추가' 대화 상대방.
         //로그인한 계정
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute("userSession", authentication.getPrincipal());
-        System.out.println("채팅 시 로그인한 계정"+authentication.getPrincipal());
+        System.out.println("채팅 시 로그인한 계정" + authentication.getPrincipal());
         return "chat/chat";
     }
 }
